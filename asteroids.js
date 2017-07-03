9b4d0dbfbb51b461982845f5db449f055ee745df
AKASH RAJ <akashraj9828@users.noreply.github.com>
2017-07-03 18:32:06 +0530

    Add files via upload

---
+++ asteroids.js
@@ -0,0 +1,67 @@
+function asteroid(posn,rad){
+
+
+  if(posn){
+    this.pos.x=posn.x;
+    this.pos.y=posn.y;
+  }else {
+    this.pos=createVector(random(width),random(height));
+  }
+if(rad){
+  this.r=rad
+
+}else{
+  this.r=random(20,80);
+}
+
+this.total=floor(random(5,20));
+  this.offset=[];
+  this.vel=p5.Vector.random2D();
+
+
+  for(var i=0;i<this.total;i++){
+    this.offset[i]=random(-15,15)
+  }
+  push();
+  this.render=function(){
+
+
+
+  beginShape();
+  push();
+stroke(3000);
+noFill();
+translate(this.pos.x,this.pos.y)
+  for (var i=0;i<this.total;i++){
+    var angle=map(i,0,this.total,0,2*PI)
+    var r=this.r+this.offset[i];
+    var x=r*cos(angle );
+    var y=r*sin(angle );
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+pop();
+  }
+
+  this.update=function(){
+    this.pos.add(this.vel);
+  }
+
+  this.edge=function(){
+    if(this.pos.x>width+this.r){
+      this.pos.x=-this.r;
+    }else if(this.pos.x<-this.r){
+      this.pos.x=width+this.r;
+    }else if(this.pos.y>height+this.r){
+      this.pos.y=-this.r;
+    }else if(this.pos.y<-this.r){
+      this.pos.y=height+this.r;
+    }
+  }
+
+
+  this.breakup=function(){
+    var newA=[]
+    newA[0]=new asteroid(this.pos,this.r)
+  }
+}
---
+++ laser.js
@@ -0,0 +1,45 @@
+function laser(spos,angle){
+this.lspeed=5;
+this.pos=createVector(spos.x,spos.y);
+this.heading=p5.Vector.fromAngle(angle);
+this.vel=createVector(this.heading.x*this.lspeed,this.heading.y*this.lspeed);
+
+
+this.update=function(){
+  this.pos.add(this.vel);
+}
+
+
+this.render=function(){
+  push();
+  stroke(200);
+  strokeWeight(20);
+
+  point(this.pos.x,this.pos.y);
+pop();
+}
+
+
+this.edge=function(){
+  if(this.pos.x>width+this.r){
+    this.pos.x=-this.r;
+  }else if(this.pos.x<-this.r){
+    this.pos.x=width+this.r;
+  }else if(this.pos.y>height+this.r){
+    this.pos.y=-this.r;
+  }else if(this.pos.y<-this.r){
+    this.pos.y=height+this.r;
+  }
+}
+
+this.hits=function(ast){
+  d=dist(this.pos.x,this.pos.y,ast.pos.x,ast.pos.y)
+if(d<ast.r)
+console.log(hit);
+
+  return false;
+
+}
+
+
+}
